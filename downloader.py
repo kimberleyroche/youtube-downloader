55cfd256b59625903d2de79971a7091ef5d89244
Kimberley Roche <kimberley.g.roche@gmail.com>
2023-11-10 01:41:28 +1000

    Add input for url

--- downloader.py
+++ downloader.py
@@ -1,13 +1,14 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
-yt = YouTube(link)
+url = input("Enter video URL here: ")
+yt = YouTube(url)
 
-print("Title": yt.title)
-
+print("Title: ", yt.title)
 print("Views: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
 
 yd.download()
+
+print("Download complete.")
