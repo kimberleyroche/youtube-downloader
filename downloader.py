0cfa698fdc5a3db95738cd630810fbcf5721ab2c
Kimberley Roche <kimberley.g.roche@gmail.com>
2023-11-10 06:24:25 +1000

    Add more info values

--- downloader.py
+++ downloader.py
@@ -5,6 +5,10 @@ yt = YouTube(url)
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
+print("Description: ", yt.description)
+print("Length: ", yt.length)
+print("Publish Date: ", yt.publish_date)
+print("Rating: ", yt.rating)
 
 yd = yt.streams.get_highest_resolution()
 
